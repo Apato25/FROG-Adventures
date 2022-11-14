9905f3e0527945900c505607e75757353f8457ae
Gabriel Abat <115327869+Apato25@users.noreply.github.com>
2022-11-14 16:13:07 -0300

    personagem funcionando

---
+++ scripts/player/Player.gd
@@ -0,0 +1,82 @@
+extends KinematicBody2D
+
+var spd = 100.0
+var player_velo = Vector2.ZERO
+onready var anim := $animFrog
+
+var se_moveu = false
+var current_state := 0
+var enter_state := true
+enum {parado,andando,atacando}
+
+func _physics_process(delta):
+	
+	
+	match current_state:
+		parado:
+			_parado()
+		andando:
+			_andando()
+	
+	if player_velo.x < 0:
+		$frogSpr.scale.x = 1
+	else:
+		$frogSpr.scale.x = -1
+
+#-----------------------------------------------
+func _parado():
+	anim.play("parado")
+	
+	_move()
+	_set_state(_check_parado())
+
+func _andando():
+	if player_velo.y >= 0:
+		anim.play("andando_de_frente")
+	elif player_velo.y <= 0:
+		anim.play("andando_de_costas")
+	
+	_move()
+	_set_state(_check_andando())
+
+#-----------------------------------------------
+
+func _check_parado():
+	var new_state = current_state
+	if se_moveu == true:
+		new_state = andando
+	return new_state
+
+
+func _check_andando():
+	var new_state = current_state
+	if se_moveu == false:
+		new_state = parado
+	return new_state
+
+
+#-----------------------------------------------
+
+func _set_state(new_state):#seleciona o novo estado do peixe
+	if new_state != current_state:
+		enter_state = true
+	current_state = new_state
+
+#-----------------------------------------------
+
+func _get_direction(): #pega a posição que o player vai se mover
+	return Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
+
+func _move(): #aplica velocidade no player o fazendo se mover para a direção indicada
+	var dir = _get_direction()
+	player_velo = lerp(player_velo,dir.normalized() * spd,0.3)
+	
+	if dir.x != 0 or dir.y != 0:
+		se_moveu = true
+	else:
+		se_moveu = false
+	
+	move_and_slide(player_velo)
