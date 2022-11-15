d7b9b9c4223514023aa74e577a286fe01a7608b7
Apato25 <115327869+Apato25@users.noreply.github.com>
2022-11-15 13:49:17 -0300

    sprites e animações
    melhores animações

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -4,13 +4,13 @@ var spd = 100.0
 var player_velo = Vector2.ZERO
 onready var anim := $animFrog
 
-var se_moveu = false
+var se_moveu := false
 var current_state := 0
 var enter_state := true
 enum {parado,andando,atacando}
 
 func _physics_process(delta):
-	
+	print(player_velo)
 	
 	match current_state:
 		parado:
@@ -18,23 +18,29 @@ func _physics_process(delta):
 		andando:
 			_andando()
 	
-	if player_velo.x < 0:
+	if player_velo.x <= 0:
 		$frogSpr.scale.x = 1
 	else:
 		$frogSpr.scale.x = -1
+	
+	
 
 #-----------------------------------------------
+
 func _parado():
 	anim.play("parado")
 	
 	_move()
 	_set_state(_check_parado())
 
+
 func _andando():
-	if player_velo.y >= 0:
-		anim.play("andando_de_frente")
-	elif player_velo.y <= 0:
+	if int(player_velo.y) > 0.0:
+		anim.play("andando_para_baixo")
+	elif int(player_velo.y) < 0.0 :
 		anim.play("andando_de_costas")
+	elif int(player_velo.y) == 0.0:
+		anim.play("andando_de_frente")
 	
 	_move()
 	_set_state(_check_andando())
@@ -57,7 +63,7 @@ func _check_andando():
 
 #-----------------------------------------------
 
-func _set_state(new_state):#seleciona o novo estado do peixe
+func _set_state(new_state): #seleciona o novo estado do peixe
 	if new_state != current_state:
 		enter_state = true
 	current_state = new_state
@@ -72,7 +78,7 @@ func _get_direction(): #pega a posição que o player vai se mover
 
 func _move(): #aplica velocidade no player o fazendo se mover para a direção indicada
 	var dir = _get_direction()
-	player_velo = lerp(player_velo,dir.normalized() * spd,0.3)
+	player_velo = lerp(player_velo,dir.normalized() * spd, 0.2)
 	
 	if dir.x != 0 or dir.y != 0:
 		se_moveu = true
