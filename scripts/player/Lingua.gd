9954e882117d340433b1767f0f0d9b85eed219c8
Gabriel Abat <115327869+Apato25@users.noreply.github.com>
2022-11-16 12:38:07 -0300

    tudo esta pronto

--- scripts/player/Lingua.gd
+++ scripts/player/Lingua.gd
@@ -9,7 +9,7 @@ var mouse_pos
 func _physics_process(delta):
 	mouse_pos = get_local_mouse_position()
 	
-	if Input.is_action_pressed("Attack") == true:
+	if Input.is_action_pressed("Attack") == true and Global.can_attack == true:
 		posLingua.position = posLingua.position.linear_interpolate(mouse_pos.limit_length(reach), delta * 20)
 		lingua.set_point_position(1,posLingua.position)
 	
