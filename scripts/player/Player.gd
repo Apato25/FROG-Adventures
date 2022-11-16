5e3b16c72bb6d58b1b2be201eebe8d5200960601
Gabriel Abat <115327869+Apato25@users.noreply.github.com>
2022-11-16 08:43:14 -0300

    tentando merge

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -9,7 +9,7 @@ var current_state := 0
 var enter_state := true
 enum {parado,andando,atacando}
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	print(player_velo)
 	
 	match current_state:
@@ -18,12 +18,8 @@ func _physics_process(delta):
 		andando:
 			_andando()
 	
-	if player_velo.x <= 0:
-		$frogSpr.scale.x = 1
-	else:
-		$frogSpr.scale.x = -1
-	
-	
+	$frogSpr.scale.x = 1 if player_velo.x <= 0 else -1
+	_set_state(_check_state())
 
 #-----------------------------------------------
 
@@ -31,41 +27,27 @@ func _parado():
 	anim.play("parado")
 	
 	_move()
-	_set_state(_check_parado())
+#	_set_state(_check_parado())
 
 
 func _andando():
-	if int(player_velo.y) > 0.0:
-		anim.play("andando_para_baixo")
-	elif int(player_velo.y) < 0.0 :
-		anim.play("andando_de_costas")
-	elif int(player_velo.y) == 0:
-		anim.play("andando_de_frente")
-	
+	var animation = (
+		"andando_para_baixo" if int(player_velo.y) > 0
+		else "andando_de_costas" if int(player_velo.y) < 0 
+		else "andando_de_frente"
+	)
+	anim.play(animation)
 	_move()
-	_set_state(_check_andando())
 
 #-----------------------------------------------
 
-func _check_parado():
-	var new_state = current_state
-	if se_moveu == true:
-		new_state = andando
+func _check_state():
+	var new_state = andando if se_moveu else parado
 	return new_state
-
-
-func _check_andando():
-	var new_state = current_state
-	if se_moveu == false:
-		new_state = parado
-	return new_state
-
 
 #-----------------------------------------------
 
 func _set_state(new_state): #seleciona o novo estado do peixe
-	if new_state != current_state:
-		enter_state = true
 	current_state = new_state
 
 #-----------------------------------------------
@@ -78,11 +60,6 @@ func _get_direction(): #pega a posição que o player vai se mover
 
 func _move(): #aplica velocidade no player o fazendo se mover para a direção indicada
 	var dir = _get_direction()
-	player_velo = lerp(player_velo,dir.normalized() * spd, 0.5)
-	
-	if dir.x != 0 or dir.y != 0:
-		se_moveu = true
-	else:
-		se_moveu = false
-	
-	move_and_slide(player_velo)
+	player_velo = lerp(player_velo,dir.normalized() * spd, 0.2)
+	se_moveu = 1 if dir else 0
+	player_velo = move_and_slide(player_velo)
