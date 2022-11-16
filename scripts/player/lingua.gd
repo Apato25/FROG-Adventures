ea0eaf5b8deaa5a1ab9b58512adb18929f484ae9
Gabriel Abat <115327869+Apato25@users.noreply.github.com>
2022-11-16 00:05:43 -0300

    lingua quase pronta

--- scripts/player/lingua.gd
+++ scripts/player/lingua.gd
@@ -1,19 +1,21 @@
 extends Node2D
 
+onready var lingua = $lingua
 onready var posLingua = $pos_lingua
 onready var inipos = $pos_inicio
-var can_attack = true
 var mouse_pos 
 
 func _physics_process(delta):
 	mouse_pos = get_local_mouse_position()
 	
-	if Input.is_action_pressed("Attack") == true and  can_attack == true:
-		posLingua.position = posLingua.position.linear_interpolate(mouse_pos, delta * 25)
+	if Input.is_action_pressed("Attack") == true:
+		posLingua.position = posLingua.position.linear_interpolate(mouse_pos.limit_length(60), delta * 20)
+		lingua.set_point_position(1,posLingua.position)
 		
 	
 	if Input.is_action_pressed("Attack") == false:
-			posLingua.position = posLingua.position.linear_interpolate(inipos.position, delta * 25)
-			
-			
-			print(can_attack)
+		posLingua.position = posLingua.position.linear_interpolate(inipos.position, delta * 20)
+		lingua.set_point_position(1,posLingua.position)
+	
+
+
