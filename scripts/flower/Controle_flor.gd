9f7a9fe69417cd1a266204a8eddc32273fdbec9d
Apato25 <115327869+Apato25@users.noreply.github.com>
2022-11-22 11:02:52 -0300

    adicionando sprites novas

--- scripts/flower/Controle_flor.gd
+++ scripts/flower/Controle_flor.gd
@@ -1,7 +1,6 @@
 extends TextureProgress
 
 
-
 func _physics_process(delta):
-	max_value = get_tree().root.get_node("Flor").flor_max_life
-	value = get_tree().root.get_node("Flor").current_life
+	max_value = get_tree().get_current_scene().get_node("Flor").flor_max_life
+	value = get_tree().get_current_scene().get_node("Flor").current_life
